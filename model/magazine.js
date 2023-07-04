e42ca329c594a56ed84e179c625268f4935f2ca5
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-04 08:59:09 +0200

    new file

--- model/magazine.js
+++ model/magazine.js
@@ -1,13 +1,11 @@
-class Magazine{
+class Magazine extends Publication{
     constructor(title, dop, publisher, issue){
-        this.title = title;
-        this.dop = dop;
-        this.publisher = publisher;
+        super(title, dop, publisher);
         this.issue = issue;
     }
 
     toString() {
-        const card = 'Titolo: ' + this.title + '\n' + 'Data di pubblicazione: ' + this.dop + '\n' + 'Casa editrice: ' + this.publisher + '\n' + 'Numero carta: ' + this.issue + '\n';
+        const card = super.toString() + 'Numero carta: ' + this.issue + '\n';
         
         return card;
     }
